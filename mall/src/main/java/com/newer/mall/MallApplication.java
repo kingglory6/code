69073029c7ea1ada46c3bd05c8a6e4c14a7cf19f
one1hero <one1hero@users.noreply.github.com>
2019-04-23 16:59:03 +0800

    添加定时删除临时图片

--- mall/src/main/java/com/newer/mall/MallApplication.java
+++ mall/src/main/java/com/newer/mall/MallApplication.java
@@ -3,9 +3,11 @@ package com.newer.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.newer.mall.common.mapper")
 @SpringBootApplication
+@EnableScheduling
 public class MallApplication {
 
 	public static void main(String[] args) {
---
+++ mall/src/main/java/com/newer/mall/common/utils/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.newer.mall.common.utils;
+
+import java.io.File;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+	private File file = new File("G:/image/temp");
+	
+	@Scheduled(fixedRate = 1000*60*60*24)
+	 public void deleteTemp() {
+		File[] files = file.listFiles();
+		for(File f:files) {
+			f.delete();
+		}
+	 }
+	
+	
+
+}
